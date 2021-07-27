e27877899f32cdad33b935bb3b4153ba998db832
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-07-27 17:19:38 +0900

    fix file name

--- src/app.py
+++ src/app.py
@@ -21,11 +21,11 @@ def index():
     session['bot_name'] = 'U Roid Chat'
     session['negaposi']=0
     session['np_ALL']=0
-    return render_template("home.html")
-
-@app.route("/index", methods=["POST"])
-def move_index():
     return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def move_index():
+    return render_template("chat.html")
 
 @app.route('/rename', methods=['POST'])
 def rename():
