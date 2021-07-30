c5e2da4530347f9ad064b5fa5a09468fe3da45e9
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-07-30 21:08:37 +0900

    fix .gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
 
     output: {
-        path: path.resolve(__dirname, 'src/static/build'),
+        path: path.resolve(__dirname, 'src/static/webpack'),
         filename: 'build_[name].js'
     },
 
