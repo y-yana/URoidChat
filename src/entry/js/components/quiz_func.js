acaebfb446b21d00dde4a712312ec7656039239f
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-08-28 16:29:54 +0900

    quizName get success

--- src/entry/js/components/quiz_func.js
+++ src/entry/js/components/quiz_func.js
@@ -1,1 +1,14 @@
-console.log('quiz');
+const quiz = new Vue({
+  el: '#quiz',
+  // FlaskとVueを共存させるためにDelimiterを変更する
+  delimiters: ["[[", "]]"],
+  data: {
+    select: true,
+    option: true
+  },
+  methods: {
+    createQuiz: function (value) {
+      console.log(value)
+    }
+  }
+});
