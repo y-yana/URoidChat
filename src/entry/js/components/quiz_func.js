63258cf3b3bd3c31da159322a2ae416152baa115
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-08-28 19:55:59 +0900

    add createArr & shuffleArr

--- src/entry/js/components/quiz_func.js
+++ src/entry/js/components/quiz_func.js
@@ -6,21 +6,49 @@ const quiz = new Vue({
     select: true,
     quizNameOption: true,
     info: null,
-    quiz: null
+    quiz: null,
+    quizNumArr: [],
+    i: 0,
+    j: 0,
+    n: 37,
+    temp: 0
   },
   mounted() {
     axios
       .get('./static/json/quiz.json')
       .then(response => { this.info = response.data.quiz })
+    this.createArr()
   },
   methods: {
+    createArr: function () {
+      var i = this.i
+      this.quizNumArr.splice(0, this.quizNumArr.length)
+      for (i = 0; i < 37; i++){
+        this.quizNumArr.push(i);
+      }
+      console.log(this.quizNumArr)
+    },
     createQuiz: function (value) {
       this.getQuizData(value)
+      this.shuffleArr()
     },
     getQuizData: function (value) {
       axios
         .get('./static/json/' + this.info[value].quizName + '.json')
         .then(response => { this.quiz = response.data })
+    },
+    shuffleArr: function () {
+      var n = this.n
+      var temp = this.temp
+      var j = this.j
+      var arr = this.quizNumArr
+      while (n) {
+        j = Math.floor(Math.random() * n--);
+        temp = arr[n];
+        arr[n] = arr[j];
+        arr[j] = temp;
+      }
+      console.log(this.quizNumArr)
     }
   }
 });
