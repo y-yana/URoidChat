275d34a30118670981aa74c26eb99e1f901c7989
Leon Kunishi <49759672+Kamenleon@users.noreply.github.com>
2021-09-01 21:00:28 +0900

    一時保存

--- src/app.py
+++ src/app.py
@@ -24,11 +24,12 @@ def index():
     session['bot_name'] = 'U Roid Chat'
     session['negaposi']=0
     session['np_ALL']=0
-    return render_template("index.html", pageTitle='TopPage')
+    #return render_template("index.html", pageTitle='TopPage')
 
 @app.route("/chat", methods=["POST"])
 def move_index():
-    return render_template("chat.html", pageTitle='URoidChat')
+    #return render_template("chat.html", pageTitle='URoidChat')
+    return render_template("draw.html", pageTitle='URoidDraw')
 
 @app.route('/rename', methods=['POST'])
 def rename():
