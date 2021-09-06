dc160447a115cc83830412d6c8d81932330440f3
Ryunosuke-Ikeda <ryunosuke0305@gmail.com>
2021-09-06 21:16:12 +0900

    update

--- src/python/shiritori.py
+++ src/python/shiritori.py
@@ -3,7 +3,7 @@ import pandas as pd
 def shiritori(end_word):
   if end_word=="ん":
     return ""
-
+    
   else:
     df2=pd.read_csv("./static/shiritori/vocab.csv")
     res=df2[df2["WEBJRGA"].str.startswith(str(end_word))].sample(n=1)
