61e138158ea3d8278249a2cafeef49e83d53b2ff
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-09-14 19:24:28 +0900

    fix leaveButton

--- src/entry/js/components/oekaki_func.js
+++ src/entry/js/components/oekaki_func.js
@@ -1,6 +1,17 @@
 document.getElementById('enterTheRoom').onclick = function () {
+  // room選択画面の非表示
   var roomChoiceArea = document.getElementById('roomChoiceArea');
   roomChoiceArea.style.display = 'none';
+  // topに戻るボタンの非表示
+  var topMoveBtn = document.getElementById('topMoveBtn');
+  topMoveBtn.style.display = 'none';
+  // room選択画面に戻るボタンの表示
+  var leaveButton = document.getElementById('leaveButton');
+  leaveButton.style.display = 'block';
+  // room名&ニックネームの表示
+  var messageHeader = document.getElementById('messageHeader');
+  messageHeader.style.display = 'block';
+  // おえかき画面の表示
   var oekakiRoomArea = document.getElementById('oekakiRoomArea');
   oekakiRoomArea.style.display = 'block';
 }
