1276cf9dd4b2f8caab2a05cd6b19a73a43c4ca3a
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-09-17 19:38:09 +0900

    opening ajax success!!!!!!

--- src/entry/js/components/top_opening.js
+++ src/entry/js/components/top_opening.js
@@ -7,12 +7,29 @@ function fadeout() {
   $('#openingForm').fadeOut('slow');
 }
 
-document.getElementById('openingBtn').onclick = function openingForm() {
-  const newName = $('#openingForm').serialize();
+$("#openingBtn").on('click', function(){
+  var soundData = document.querySelector("input[name=sound]");
+  var yourNameData = document.querySelector("input[name=yourName]");
+  var AInameData = document.querySelector("input[name=AIname]");
+  var vrmFileData = document.querySelector("input[name=vrmFile]");
+
+  var formData = {
+    sound: soundData.checked, // boolean
+    yourName: yourNameData.value, // str
+    AIname: AInameData.value, // str
+    vrmFile: vrmFileData.value // str
+  };
+
+  var postFormData = JSON.stringify(formData);
+
   $.ajax("/opening", {
     type: "post",
-    data: newName,
+    data: postFormData,
     dataType: "json",
   });
+
   setTimeout(fadeout, 900);
-}
+
+  // ここにsoundがtrueの場合に音声を再生させる処理を書く
+
+  })
